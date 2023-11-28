7fac15523038963b8ae7d410ec08bc46c042f54a
ismelb <stivok1@mail.ru>
2023-11-28 16:05:32 +0100

    reworked header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,30 @@
+'use client';
+
 import { FC } from 'react';
+import { usePathname } from 'next/navigation';
+import SearchBar from '../searchBar/SearchBar';
+import HeaderNavigation from '../headerNavigation/HeaderNavigation';
+import Link from 'next/link';
+import PlayingSong from '../playingSong/PlayingSong';
 
-interface IHeader {}
+const Header: FC = () => {
+  const pathName = usePathname();
 
-const Header: FC<IHeader> = (props) => {
-  const a = 0;
-  return <header>header</header>;
+  return (
+    <header className="header">
+      <HeaderNavigation />
+      {pathName.includes('/search') && <SearchBar />}
+      <PlayingSong />
+      <Link
+        title="https://github.com/iSmelb?tab=repositories"
+        className="github"
+        href="https://github.com/iSmelb?tab=repositories"
+        target="_blank"
+      >
+        Look another projects
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
