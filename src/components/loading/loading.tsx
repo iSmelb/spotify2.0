e914c2d8e807bbd5a366da69ccf0e8d1374837bb
ismelb <stivok1@mail.ru>
2023-11-28 16:05:00 +0100

    reworked loader

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,9 +1,33 @@
 import { FC } from 'react';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import { Box } from '@mui/material';
 
-interface ILoading {}
+const Loading: FC = () => {
+  return (
+    <Box width="100%" display="flex" justifyContent="center">
+      <MusicNoteIcon
+        sx={{
+          width: '100px',
+          height: '100px',
+          color: 'var(--green)',
+          animation: 'pulse 1.5s infinite',
 
-const Loading: FC<ILoading> = (props) => {
-  return <div>Loading</div>;
+          '@keyframes pulse': {
+            '0%': {
+              transform: 'scale(0.8)',
+            },
+            '50%': {
+              transform: 'scale(1)',
+              boxShadow: '0 0 0 0 var(--green)',
+            },
+            '100%': {
+              transform: 'scale(0.8)',
+            },
+          },
+        }}
+      />
+    </Box>
+  );
 };
 
 export default Loading;
